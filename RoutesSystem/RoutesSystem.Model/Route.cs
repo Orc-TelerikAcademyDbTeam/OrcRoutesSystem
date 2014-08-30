9e06e811436c399e4769f286dfb2599a8e0e669a
Lyubomir Ganchev <luboganchev91@gmail.com>
2014-08-30 14:02:31 +0300

    Adding driver, route and town to code first.

---
+++ RoutesSystem/RoutesSystem.Model/Route.cs
@@ -0,0 +1,24 @@
+﻿namespace RoutesSystem.Model
+{
+    using System.Collections.Generic;
+
+    public class Route
+    {
+        public Route()
+        {
+            this.Vehicles = new HashSet<Vehicle>();
+        }
+
+        public int Id { get; set; }
+
+        public int StartTown { get; set; }
+
+        public int EndTown { get; set; }
+
+        public virtual Town Town { get; set; }
+
+        public float Distance { get; set; }
+
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ RoutesSystem/RoutesSystem.Model/Town.cs
@@ -0,0 +1,18 @@
+﻿namespace RoutesSystem.Model
+{
+    using System.Collections.Generic;
+
+    public class Town
+    {
+        public Town()
+        {
+            this.Routes = new HashSet<Route>();
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public virtual ICollection<Route> Routes { get; set; }
+    }
+}
