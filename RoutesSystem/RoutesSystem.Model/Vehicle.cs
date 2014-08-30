ddcdbd9145e5715503faa23c0637868cbc636b1c
juliameleshko <julia.meleshko@yahoo.com>
2014-08-30 13:22:41 +0300

    added tables Vehicle and VehicleType

---
+++ RoutesSystem/RoutesSystem.Model/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿namespace RoutesSystem.Model
+{
+    using System;
+
+    class Vehicle
+    {
+        public int Id { get; set; }
+
+        //public int DriverId { get; set; }
+
+        //public virtual Driver Driver { get; set; }
+
+        public int VehicleTypeId { get; set; }
+
+        public virtual VehicleType Type { get; set; }
+
+        public int ManifacturerId {get;set;}
+
+        public int ModelId { get; set; }
+
+        public DateTime YearOfManifacturer { get; set; }
+
+        public int FuelTypeId { get; set; }
+        
+    }
+}
---
+++ RoutesSystem/RoutesSystem.Model/VehicleType.cs
@@ -0,0 +1,14 @@
+﻿namespace RoutesSystem.Model
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class VehicleType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
