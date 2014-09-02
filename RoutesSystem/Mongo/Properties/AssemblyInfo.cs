7d88bc0a4caa0e105271c7e1955793028273840c
Geno Genov <g.genov1993@gmail.com>
2014-09-02 11:55:50 +0300

    Initial MongoDB Project

--- RoutesSystem/Mongo/Properties/AssemblyInfo.cs
+++ RoutesSystem/Mongo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MongoDB")]
+[assembly: AssemblyTitle("Mongo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MongoDB")]
+[assembly: AssemblyProduct("Mongo")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31bd3ab4-0384-45df-866a-a9b39bf2754b")]
+[assembly: Guid("b89c8f02-ed54-48d3-837e-9631f9578e52")]
 
 // Version information for an assembly consists of the following four values:
 //
